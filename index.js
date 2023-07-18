2780808efa740e9a991a99bca4c97a6684e4a306
Air-is-unfair <95660750+Air-is-unfair@users.noreply.github.com>
2023-07-19 01:22:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    if(width >= 523){
+     console.log("correct");
+    } else {
+          console.log("wrong")
+    }
+});
